2742f9ec9fa218909336112728f06b0f94ce40be
Iliasg <iliasg@telenet.be>
2022-02-02 23:31:00 +0100

    Tracker - Add use case wrapper

---
+++ tracker/tracker_domain/src/main/kotlin/com/iliasg/tracker_domain/use_case/TrackerUseCaseWrapper.kt
@@ -0,0 +1,9 @@
+package com.iliasg.tracker_domain.use_case
+
+data class TrackerUseCaseWrapper(
+    val trackFood: TrackFood,
+    val searchFood: SearchFood,
+    val getFoodsForDate: GetFoodsForDate,
+    val deleteTrackedFood: DeleteTrackedFood,
+    val calculateMealNutrients: CalculateMealNutrients
+)
