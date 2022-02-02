b82e0ac24c3353afd7f819fd58d2ee0c1214c70c
Iliasg <iliasg@telenet.be>
2022-02-02 23:01:16 +0100

    Create app and setup

--- app/src/main/kotlin/com/iliasg/calorietracker/MainActivity.kt
+++ app/src/main/kotlin/com/iliasg/calorietracker/MainActivity.kt
@@ -3,13 +3,151 @@ package com.iliasg.calorietracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import coil.annotation.ExperimentalCoilApi
+import com.iliasg.calorietracker.navigation.Route
 import com.iliasg.calorietracker.ui.theme.CalorieTrackerTheme
+import com.iliasg.core.domain.preferences.Preferences
+import com.iliasg.onboarding_presentation.activity.ActivityScreen
+import com.iliasg.onboarding_presentation.age.AgeScreen
+import com.iliasg.onboarding_presentation.gender.GenderScreen
+import com.iliasg.onboarding_presentation.goal.GoalScreen
+import com.iliasg.onboarding_presentation.height.HeightScreen
+import com.iliasg.onboarding_presentation.nutrient_goal.NutrientGoalScreen
+import com.iliasg.onboarding_presentation.weight.WeightScreen
+import com.iliasg.onboarding_presentation.welcome.WelcomeScreen
+import com.iliasg.tracker_presentation.search.SearchScreen
+import com.iliasg.tracker_presentation.tracker_overview.TrackerOverviewScreen
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@ExperimentalCoilApi
+@ExperimentalComposeUiApi
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var preferences: Preferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // True if the user data is not cached yet.
+        val shouldShowOnboarding = preferences.loadShouldShowOnboarding()
+
         setContent {
             CalorieTrackerTheme {
+                val navController = rememberNavController()
+                val scaffoldState = rememberScaffoldState()
+
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
+                ) {
+                    // All possible screen routes.
+                    NavHost(
+                        navController = navController,
+                        startDestination = if(shouldShowOnboarding) {
+                            Route.WELCOME
+                        } else Route.TRACKER_OVERVIEW
+                    ) {
+                        composable(Route.WELCOME) {
+                            WelcomeScreen(onNextClick = {
+                                navController.navigate(Route.GENDER)
+                            })
+                        }
+                        composable(Route.AGE) {
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNextClick = {
+                                    navController.navigate(Route.HEIGHT)
+                                }
+                            )
+                        }
+                        composable(Route.GENDER) {
+                            GenderScreen(onNextClick = {
+                                navController.navigate(Route.AGE)
+                            })
+                        }
+                        composable(Route.HEIGHT) {
+                            HeightScreen(
+                                scaffoldState = scaffoldState,
+                                onNextClick = {
+                                    navController.navigate(Route.WEIGHT)
+                                }
+                            )
+                        }
+                        composable(Route.WEIGHT) {
+                            WeightScreen(
+                                scaffoldState = scaffoldState,
+                                onNextClick = {
+                                    navController.navigate(Route.ACTIVITY)
+                                }
+                            )
+                        }
+                        composable(Route.NUTRIENT_GOAL) {
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNextClick = {
+                                    navController.navigate(Route.TRACKER_OVERVIEW)
+                                }
+                            )
+                        }
+                        composable(Route.ACTIVITY) {
+                            ActivityScreen(onNextClick = {
+                                navController.navigate(Route.GOAL)
+                            })
+                        }
+                        composable(Route.GOAL) {
+                            GoalScreen(onNextClick = {
+                                navController.navigate(Route.NUTRIENT_GOAL)
+                            })
+                        }
+
+                        composable(Route.TRACKER_OVERVIEW) {
+                            TrackerOverviewScreen(
+                                onNavigateToSearch = { mealName, day, month, year ->
+                                    navController.navigate(
+                                        Route.SEARCH +
+                                                "/$mealName" +
+                                                "/$day" +
+                                                "/$month" +
+                                                "/$year"
+                                    )
+                                }
+                            )
+                        }
+                        composable(
+                            route = Route.SEARCH + "/{mealName}/{dayOfMonth}/{month}/{year}",
+                            arguments = listOf(
+                                navArgument("mealName") { type = NavType.StringType },
+                                navArgument("dayOfMonth") { type = NavType.IntType },
+                                navArgument("month") { type = NavType.IntType },
+                                navArgument("year") { type = NavType.IntType },
+                            )
+                        ) {
+                            SearchScreen(
+                                scaffoldState = scaffoldState,
+                                mealName = it.arguments?.getString("mealName")!!,
+                                dayOfMonth = it.arguments?.getInt("dayOfMonth")!!,
+                                month = it.arguments?.getInt("month")!!,
+                                year = it.arguments?.getInt("year")!!,
+                                onNavigateUp = {
+                                    navController.navigateUp()
+                                }
+                            )
+                        }
+                    }
+                }
 
             }
         }
