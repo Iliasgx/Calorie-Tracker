8bc86b2e90a287b283fab192a2027b4e40d0b99e
Iliasg <iliasg@telenet.be>
2022-02-02 23:11:36 +0100

    Onboarding - Create goal screen

---
+++ onboarding/onboarding_presentation/src/main/kotlin/com/iliasg/onboarding_presentation/goal/GoalViewModel.kt
@@ -0,0 +1,38 @@
+package com.iliasg.onboarding_presentation.goal
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.iliasg.core.domain.models.GoalType
+import com.iliasg.core.domain.preferences.Preferences
+import com.iliasg.core.utils.UiEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class GoalViewModel @Inject constructor(
+    private val preferences: Preferences
+): ViewModel() {
+
+    var selectedGoal by mutableStateOf<GoalType>(GoalType.KeepWeight)
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onGoalTypeSelect(goalType: GoalType) {
+        selectedGoal = goalType
+    }
+
+    fun onNextClick() {
+        viewModelScope.launch {
+            preferences.saveGoalType(selectedGoal)
+            _uiEvent.send(UiEvent.Success)
+        }
+    }
+}
