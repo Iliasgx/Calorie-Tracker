cf8b3e7a373d0a445295c1e7ef4a8db87b37ad45
Iliasg <iliasg@telenet.be>
2022-02-02 23:11:06 +0100

    Onboarding - Create activity screen

---
+++ onboarding/onboarding_presentation/src/main/kotlin/com/iliasg/onboarding_presentation/activity/ActivityViewModel.kt
@@ -0,0 +1,38 @@
+package com.iliasg.onboarding_presentation.activity
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.iliasg.core.domain.models.ActivityLevel
+import com.iliasg.core.domain.preferences.Preferences
+import com.iliasg.core.utils.UiEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ActivityViewModel @Inject constructor(
+    private val preferences: Preferences
+): ViewModel() {
+
+    var selectedActivityLevel by mutableStateOf<ActivityLevel>(ActivityLevel.Medium)
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onActivityLevelSelect(activityLevel: ActivityLevel) {
+        selectedActivityLevel = activityLevel
+    }
+
+    fun onNextClick() {
+        viewModelScope.launch {
+            preferences.saveActivityLevel(selectedActivityLevel)
+            _uiEvent.send(UiEvent.Success)
+        }
+    }
+}
